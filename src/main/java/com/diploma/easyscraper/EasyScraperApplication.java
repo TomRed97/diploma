ce5f90192bcc58c85ff124435f1be0cfd384507e
Tom Red <taprikyan@gmail.com>
2018-04-15 21:38:58 +0400

    Project setup done, home page added, styles added.

---
+++ src/main/java/com/diploma/easyscraper/EasyScraperApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.easyscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class EasyScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyScraperApplication.class, args);
+	}
+}
